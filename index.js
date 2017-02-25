34b176fbe937db80901269fd0f7ac9db0d2ca501
Daniël Illouz <daniel.illouz@me.com>
2017-02-26 00:11:10 +0100

    Implement body parser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const http = require('http');
+const contentType = require('content-type');
+const getRawBody = require('raw-body');
 
 function _status(code) {
 	this.statusCode = code;
@@ -39,5 +41,39 @@ module.exports = {
 		});
 
 		return server;
+	},
+
+	async bodyParser(req, limit = '1mb') {
+		try {
+			const length = req.headers['content-length'];
+			const encoding = contentType.parse(req).parameters.charset;
+
+			const options = {
+				limit,
+			    length,
+			    encoding
+			};
+
+			const str = await getRawBody(req, options);
+			const json = JSON.parse(str);
+
+			return json;
+		} catch (err) {
+			// TODO: error handling
+
+			if (err.type === 'entity.too.large') {
+				// 413
+				console.log(
+					`Oh dear! The request body exceeded the ${limit} limit.`,
+					err
+				);
+			} else {
+				// 400
+				console.log(
+					`Oh my! You've sent invalid JSON.`,
+					err
+				);
+			}
+		}
 	}
 };
