3af6755da74de063ff974136bb780c16f87d1426
Daniël Illouz <daniel.illouz@me.com>
2017-02-26 00:08:52 +0100

    Add response convenience methods

--- index.js
+++ index.js
@@ -2,9 +2,33 @@
 
 const http = require('http');
 
+function _status(code) {
+	this.statusCode = code;
+
+	return this;
+}
+
+function _json(data = { }) {
+	this.statusCode = this.statusCode || 200;
+	this.setHeader('Content-Type', 'application/json');
+	this.end(
+		JSON.stringify(data)
+	);
+}
+
+function _html(html = '') {
+	this.statusCode = this.statusCode || 200;
+	this.setHeader('Content-Type', 'text/html');
+	this.end(html);
+}
+
 module.exports = {
 	sir(handler) {
 		const server = http.createServer((req, res) => {
+			res.status = _status;
+			res.html = _html;
+			res.json = _json;
+
 			Promise
 				.resolve(handler(req, res))
 				.catch(err => {
